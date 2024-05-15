bb44430c7b9011a18c33c7f38dfbe09e494f20c9
jribon51 <151412716+jribon51@users.noreply.github.com>
2024-05-15 03:03:04 +0000

    6 puntos realizados

--- src/app.py
+++ src/app.py
@@ -27,16 +27,33 @@ def sitemap():
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
+    response_body =members
     return jsonify(response_body), 200
+
+@app.route("/member",methods=['POST'])
+def new_member():
+    body=request.get_json(silent=True)
+    if body is None:
+        return jsonify({"message":"debes enviar informacion en el body"})
+    if "first_name" not in body:
+        return jsonify({"message":"el campo first_name es obligatorio"})
+    print(body)
+    new_member_data = {
+            "id": jackson_family._generateId(),
+            "first_name": body["first_name"],
+            "last_name": jackson_family.last_name,
+            "age": body["age"],
+            "lucky_numbers": body["lucky_numbers"]
+        }
+    jackson_family.add_member(new_member_data)
+    return jsonify({"message":"nuevo miembro agregado"}),200
+
+
+
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
